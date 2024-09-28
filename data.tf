97bf73985846db6758837ab88d75e1d3a2fcea59
MNN <myatnyinyi.22394@gmail.com>
2024-09-28 07:23:43 +0000

    repair tfc

--- data.tf
+++ data.tf
@@ -1,3 +1,9 @@
+data "tfe_project" "default-project" {
+  name = "Default Project"
+  organization = "MNN-CLOUD"
+}
+
+
 data "tfe_organization" "MNN-tfc" {
   name = "MNN-CLOUD"
 }
--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 
 resource "tfe_workspace" "test" {
   name         = "my-workspace-name"
-  organization = data.tfe_organizations.MNN-tfc.name
+  organization = data.tfe_organization.MNN-tfc.name
   tag_names    = ["test", "app"]
+  project_id   = data.tfe_project.default-project
 }
